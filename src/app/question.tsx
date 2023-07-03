dc5543bf92b53b604ffc66a9706b00f36647809b
Gérémy Lubin <97472153+lubingeremy@users.noreply.github.com>
2023-07-02 21:26:36 -0400

    Update question.tsx
    Fix of precedent commit about type of header.

--- src/app/question.tsx
+++ src/app/question.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import arrowDown from '../../images/icon-arrow-down.svg'
 import { Accordion, AccordionItem as Item } from "@szhsin/react-accordion";
 
-const AccordionItem = ({ header, ...rest }: {header: any}) => (
+const AccordionItem = ({ header, ...rest }: any) => (
   <Item
     {...rest}
     header={({ state: { isEnter } }) => (
@@ -35,20 +35,20 @@ const AccordionItem = ({ header, ...rest }: {header: any}) => (
 
 export default function Questions() {
   return (
-    <Accordion className="w-full h-3/4 lg:h-4/5 flex flex-col text-xs text-grayish-blue" transition transitionTimeout={250}>
+    <Accordion className="w-full h-3/4 lg:h-4/5 flex flex-col text-xs text-grayish-blue" transition transitionTimeout={250} >
       <AccordionItem header="How many team members can I invite?">
         You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.
       </AccordionItem>
-      <AccordionItem header="What is the maximum file upload size?">
+      <AccordionItem header="What is the maximum file upload size?" >
         No more than 2GB. All files in your account must fit your allotted storage space.
       </AccordionItem>
-      <AccordionItem header="How do I reset my password?">
+      <AccordionItem header="How do I reset my password?" >
       Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.
       </AccordionItem>
-      <AccordionItem header="Can I cancel my subscription?">
+      <AccordionItem header="Can I cancel my subscription?" >
         Yes! Send us a message and we’ll process your request no questions asked.
       </AccordionItem>
-      <AccordionItem header="Do you provide additional support?">
+      <AccordionItem header="Do you provide additional support?" >
         Chat and email support is available 24/7. Phone lines are open during normal business hours.
       </AccordionItem>
     </Accordion>
